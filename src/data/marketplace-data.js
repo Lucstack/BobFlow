2be257cdc9f63ef4b9a207227d85377bc718d456
Luc van der Voorn <luc.vandervoorn@hibob.io>
2025-07-16 15:22:15 +0200

    workablefix2

--- src/data/marketplace-data.js
+++ src/data/marketplace-data.js
@@ -8,7 +8,7 @@ export const marketplaceCategories = {
     integrations: [
       {
         id: 'greenhouse',
-        name: 'Greenhouze',
+        name: 'Greenhouse',
         logoUrl: 'https://logo.clearbit.com/greenhouse.io',
         isMarketplacePartner: true,
         usedFor: 'Applicant tracking & recruiting.',
